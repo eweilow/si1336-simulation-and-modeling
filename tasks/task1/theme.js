6fa96d5bc01f1e70313a7c49ad737409c1e6e332
E. Weilow <eweilow@digitalkoppar.se>
2018-11-01 13:06:26 -0600

    Set up beginnings of presentation

---
+++ tasks/task1/theme.js
@@ -0,0 +1,13 @@
+import theme from "mdx-deck/themes";
+
+export default {
+  // extends the default theme
+  ...theme,
+  // add a custom font
+  // custom colors
+  colors: {
+    text: "#6200ff",
+    background: "#fff",
+    link: "#0ff"
+  }
+};
