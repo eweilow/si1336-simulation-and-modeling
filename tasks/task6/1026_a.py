6d1e41bb9126751607a5fdd650e27485dd149774
E. Weilow <eweilow@digitalkoppar.se>
2018-12-11 15:21:23 +0100

    Finish report

--- tasks/task6/1026_a.py
+++ tasks/task6/1026_a.py
@@ -22,3 +22,26 @@ plotData("./plots/1026/a_premultigrid_1.png", points, relaxations,
 points, relaxations, accuracy, data = loadFile("./data/1026_multigrid_1.bin")
 plotData("./plots/1026/a_multigrid_1.png", points, relaxations,
          accuracy, data, linearDimension=dimension, realTitle="With multigrid")
+
+dimension, equivalentRun, realRun, desiredAccuracy, powerOfTwoMax, powerOfTwoWanted, relaxationsPerProlongation, relaxationsPerRestriction = loadParametersFile(
+    "./data/1026_params_1_2.bin")
+
+print(dimension, equivalentRun, realRun, desiredAccuracy, powerOfTwoMax, powerOfTwoWanted,
+      relaxationsPerProlongation, relaxationsPerRestriction)
+
+points, relaxations, accuracy, data = loadFile("./data/1026_exact_1_2.bin")
+plotData("./plots/1026/a_exact_1_2.png", points, relaxations,
+         accuracy, data, linearDimension=dimension, realTitle="Exact (150000 relaxations)", postfix=False)
+
+points, relaxations, accuracy, data = loadFile("./data/1026_baseline_1_2.bin")
+plotData("./plots/1026/a_baseline_1_2.png", points, relaxations,
+         accuracy, data, linearDimension=dimension, realTitle="No multigrid")
+
+points, relaxations, accuracy, data = loadFile(
+    "./data/1026_premultigrid_1_2.bin")
+plotData("./plots/1026/a_premultigrid_1_2.png", points, relaxations,
+         accuracy, data, linearDimension=dimension, realTitle="Directly after multigrid")
+
+points, relaxations, accuracy, data = loadFile("./data/1026_multigrid_1_2.bin")
+plotData("./plots/1026/a_multigrid_1_2.png", points, relaxations,
+         accuracy, data, linearDimension=dimension, realTitle="With multigrid")
